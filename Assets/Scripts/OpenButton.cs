057593ca627958c0a76991fe7d2f117a5034a30b
Jan Vijfhuizen <jan.vijfhuizen@outlook.com>
2021-01-31 11:16:16 +0100

    Added color change

--- Assets/Scripts/OpenButton.cs
+++ Assets/Scripts/OpenButton.cs
@@ -1,12 +1,27 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(
+    typeof(LostItem), 
+    typeof(LostItemSorter), 
+    typeof(LostItemSorter))]
+[RequireComponent(typeof(SpriteRenderer))]
 public sealed class OpenButton : MonoBehaviour
 {
     [SerializeField] private UnityEvent _mouseDownEvent = null;
+    [SerializeField] private Sprite _upSprite = null, _downSprite = null;
+
+    private SpriteRenderer _renderer = null;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
 
     private void OnMouseDown()
     {
+        _renderer.sprite = _downSprite;
+
         if (!enabled)
             return;
 
@@ -15,5 +30,10 @@ public sealed class OpenButton : MonoBehaviour
         GetComponent<LostItem>().enabled = true;
         GetComponent<LostItemSorter>().enabled = true;
         enabled = false;
+    }
+
+    private void OnMouseUp()
+    {
+        _renderer.sprite = _upSprite;
     }
 }
