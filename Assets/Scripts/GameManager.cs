04c615aaefd56eb5acaa535fb94b5180f5cd246e
Jan Vijfhuizen <jan.vijfhuizen@outlook.com>
2021-01-31 14:17:20 +0100

    Implement score mechanic.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,12 +12,15 @@ public sealed class GameManager : Singleton<GameManager>
     [SerializeField] private int _lives = 5;
     [SerializeField] private Text _livesText = null;
 
+    [SerializeField] private Text _scoreText = null;
+
     [SerializeField, Tooltip("Leave empty to generate random seed")] 
     private string _seed = "";
 
     private Random _random = null;
     private int _currentLives = 0;
 
+    public static int currentScore = 0;
 
     public State GameState
     {
@@ -26,7 +29,10 @@ public sealed class GameManager : Singleton<GameManager>
         {
             bool game = value == State.Game;
             _livesText.gameObject.SetActive(game);
+            _scoreText.gameObject.SetActive(game);
+
             UpdateHealthText();
+            UpdateScoreText();
 
             _state = value;
         }
@@ -50,11 +56,17 @@ public sealed class GameManager : Singleton<GameManager>
         _random = new Random(chosenSeed);
 
         _currentLives = _lives;
+        currentScore = 0;
     }
 
     private void UpdateHealthText()
     {
         _livesText.text = $"Lives: {_currentLives}";
+    }
+
+    private void UpdateScoreText()
+    {
+        _scoreText.text = $"Score: {currentScore}";
     }
 
     public void OnDamageTaken()
@@ -64,6 +76,12 @@ public sealed class GameManager : Singleton<GameManager>
 
         if (_currentLives == 0) 
             OnGameOver();
+    }
+
+    public void OnCustomerHelped()
+    {
+        currentScore++;
+        UpdateScoreText();
     }
 
     private void OnGameOver()
